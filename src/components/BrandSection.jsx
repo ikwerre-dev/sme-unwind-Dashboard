59c24ae2d7a7f67c059de7355f0cf8a8f80f46b2
ikwerre-dev <investorhonour@gmail.com>
2024-09-28 10:35:25 +0100

    updated and added more brands to brandsection

--- src/components/BrandSection.jsx
+++ src/components/BrandSection.jsx
@@ -1,30 +1,94 @@
 import React, { useState, useEffect, Component } from "react";
 import BrandImage from '../assets/brand.png'
 import BrandImage2 from '../assets/brand-2.png'
- 
+import priceTagImage from '../assets/price-tag.png';
+import starImage from '../assets/star.png';
+import PurplePriceTagImage from '../assets/purple-price-tag.png';
+import PurplePriceTagImage2 from '../assets/purple-price-tag-2.png';
 const BrandSection = () => {
-    
+
     return (
-        <div className="bg-gray-300 p-6 md:px-10 text-white flex justify-around flex-wrap items-center ">
-            <div className="h-[7rem] align-center items-center w-[7rem]">
-                <img src={BrandImage}  className="w-full max-w-lg mx-auto" alt="" />
+        <div className="bg-[#0E1D12] text-white p-6 md:p-[8rem] pb-[8rem] py-[4rem] md:py-[8rem] font-sans relative">
+            <img src={priceTagImage} alt="Yellow Price Tag" className="absolute top-0 left-5 w-[3rem] md:w-20 h-[3rem] md:h-20" />
+            <img src={PurplePriceTagImage2} alt="Purple Price Tag" className="absolute bottom-4 left-4 w-[4rem] md:w-[6rem] h-[4rem] md:h-[6rem]" />
+            <h1 className="text-5xl font-bold wineFont mb-4">Our Sponsors</h1>
+            <p className="mb-6 font-bold my-6">Don't miss out on any activity. Check out the event schedule</p>
+
+            <div className=" py-5 ">
+
+                <h1 className="text-3xl font-bold  mb-4">Headline</h1>
+                <div className="bg-gray-300 p-6 md:px-10 text-white flex justify-around flex-wrap rounded-lg items-center ">
+                    <div className="h-[7rem] align-center items-center w-[7rem]">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+
+                </div>
             </div>
-            <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
-                <img src={BrandImage2}  className="w-full max-w-lg mx-auto" alt="" />
+            <div className=" py-5 ">
+
+                <h1 className="text-3xl font-bold  mb-4">Plantinum</h1>
+                <div className="bg-gray-300 p-6 md:px-10 text-white flex justify-around flex-wrap rounded-lg items-center ">
+                    <div className="h-[7rem] align-center items-center w-[7rem]">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+
+                </div>
             </div>
-            <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
-                <img src={BrandImage}  className="w-full max-w-lg mx-auto" alt="" />
+            <div className=" py-5 ">
+
+                <h1 className="text-3xl font-bold  mb-4">Bronze</h1>
+                <div className="bg-gray-300 p-6 md:px-10 text-white flex justify-around flex-wrap rounded-lg items-center ">
+                    <div className="h-[7rem] align-center items-center w-[7rem]">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+                    <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
+                        <img src={BrandImage2} className="w-full max-w-lg mx-auto" alt="" />
+                    </div>
+
+                </div>
             </div>
-            <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
-                <img src={BrandImage2}  className="w-full max-w-lg mx-auto" alt="" />
-            </div>
-            <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
-                <img src={BrandImage}  className="w-full max-w-lg mx-auto" alt="" />
-            </div>
-            <div className="h-[7rem] w-[7rem] grid align-center justify-center  items-center">
-                <img src={BrandImage2}  className="w-full max-w-lg mx-auto" alt="" />
-            </div>
-           
         </div>
     );
 };
